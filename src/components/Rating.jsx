6829be18f298f159d7a2d03ccb63616a2dd165a8
alexStetsyuk <san9istec@gmail.com>
2022-11-04 10:25:09 +0200

    add star voting possibility

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import { ContentWrapper } from '../shared/ContentWrapper'
 import { HeaderThree } from '../shared/HeaderThree'
 import Star from './Star'
 
 export function Rating({ title }) {
+  const [rating, setRating] = useState(0)
   return (
     <ContentWrapper flexDirection='column' alignItems='center' gap='5px'>
       <HeaderThree>{title}</HeaderThree>
-      <Star count={4} />
+      <Star rating={rating} count={5} onRating={(rate) => setRating(rate)} />
     </ContentWrapper>
   )
 }
